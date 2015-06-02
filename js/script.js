30e0b8e79961881b1bd49a5205fb30c861509fe8
Heithvaldr <heithvaldr@gmail.com>
2015-06-02 18:25:37 +0300

    some slider manipulations: created form and designed radio buttons, now they work correct

---
+++ js/script.js
@@ -0,0 +1,7 @@
+var deviceAgent = navigator.userAgent.toLowerCase();
+    var iOS = deviceAgent.match(/(iphone|ipod|ipad)/);
+    if (iOS) {
+        $('label').click(function (event) {
+            $('#' + $(event.target).attr('for')).attr('checked', true).change();
+        });
+    }
