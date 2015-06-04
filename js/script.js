8ce471552c4002da25eeab0660dfea32bfcc4a77
Heithvaldr <heithvaldr@gmail.com>
2015-06-04 19:04:48 +0300

    added overlay to users, created rating.

--- js/script.js
+++ js/script.js
@@ -1,10 +1,3 @@
-var deviceAgent = navigator.userAgent.toLowerCase();
-    var iOS = deviceAgent.match(/(iphone|ipod|ipad)/);
-    if (iOS) {
-        $('label').click(function (event) {
-            $('#' + $(event.target).attr('for')).attr('checked', true).change();
-        });
-    }
 
 $("li").click(function(e) {
   e.preventDefault();
